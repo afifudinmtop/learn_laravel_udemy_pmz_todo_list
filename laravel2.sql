5d065e136fad9704262f03c5e5ad91591a16a264
Afifudin Maarif <32411703+afifudinmtop@users.noreply.github.com>
2023-04-12 14:20:37 +0700

    v1.10.1
    update database

--- laravel2.sql
+++ laravel2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 12, 2023 at 07:16 AM
+-- Generation Time: Apr 12, 2023 at 09:20 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `laravel2`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `todo`
+--
+
+CREATE TABLE `todo` (
+  `id` int(11) NOT NULL,
+  `todo` text DEFAULT NULL,
+  `user_id` text DEFAULT NULL,
+  `hapus` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `todo`
+--
+
+INSERT INTO `todo` (`id`, `todo`, `user_id`, `hapus`) VALUES
+(1, 'tes11', '1', 'hapus'),
+(2, 'tes 2', '1', NULL),
+(3, 'tes 3', '1', 'hapus');
 
 -- --------------------------------------------------------
 
@@ -46,6 +68,12 @@ INSERT INTO `user` (`id`, `username`, `password`) VALUES
 --
 
 --
+-- Indexes for table `todo`
+--
+ALTER TABLE `todo`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -54,6 +82,12 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `todo`
+--
+ALTER TABLE `todo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `user`
