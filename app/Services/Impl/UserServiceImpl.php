13dd7bee0002560f9d2ec406bfee22075bf81c6e
Afifudin Maarif <32411703+afifudinmtop@users.noreply.github.com>
2023-04-12 10:02:08 +0700

    v1.2.0
    logic login, template

--- app/Services/Impl/UserServiceImpl.php
+++ app/Services/Impl/UserServiceImpl.php
@@ -5,5 +5,17 @@ namespace App\Services\Impl;
 use App\Services\UserService;
 
 class UserServiceImpl implements UserService{
-    
+    private array $users = [
+        "apip" => "udin"
+    ];
+
+    public function login($user, $password)
+    {
+        if (!isset($this->users[$user])) {
+            return false;
+        }
+
+        $correctPassword = $this->users[$user];
+        return $password == $correctPassword;
+    }
 }
